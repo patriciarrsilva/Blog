0c49e1869c373285a5776fb234d0790447538961
Patrícia <prrs89@gmail.com>
2019-08-17 16:46:38 +0100

    Updates

---
+++ precache-manifest.05afc5420bd1f18c7745188e34e275a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2561efa6c4dfc68a675fc4260a98bd81",
+    "url": "/Blog/index.html"
+  },
+  {
+    "revision": "1c8064f2bb5de570949e",
+    "url": "/Blog/static/css/main.847f000c.chunk.css"
+  },
+  {
+    "revision": "f5398739988470aad23c",
+    "url": "/Blog/static/js/2.6274e303.chunk.js"
+  },
+  {
+    "revision": "1c8064f2bb5de570949e",
+    "url": "/Blog/static/js/main.061e3dfc.chunk.js"
+  },
+  {
+    "revision": "e0432beeeb2c274a35b1",
+    "url": "/Blog/static/js/runtime~main.1b68dcfb.js"
+  },
+  {
+    "revision": "c0c5c509a7483ce0b8c11ca04f3633f1",
+    "url": "/Blog/static/media/arrow-back.c0c5c509.svg"
+  },
+  {
+    "revision": "32cee4372a4c9744f7e703013278c61d",
+    "url": "/Blog/static/media/cancel.32cee437.svg"
+  }
+]);
